daceabdb246bc5b38f27ea53ef54eb5540fd5c38
Toni Colom <toni.colom@elementsinteractive.es>
2017-11-13 18:19:19 +0100

    Added tests for Entry model

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+
+class EntryTestCase(TestCase):
+    def test_fields(self):
+        entry = Entry.objects.create(
+            title="Test",
+            description="Test test"
+        )
+        self.assertEqual(entry.title, "Test")
+        self.assertEqual(entry.description, "Test test")
+        self.assertEqual(entry.slug, "test")
