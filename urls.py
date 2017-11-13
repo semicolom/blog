687712038ad53ac0e82f4c7cbed03714e54021fb
Toni Colom <toni.colom@elementsinteractive.es>
2017-11-13 18:59:27 +0100

    Added admin for Entry and home template

--- urls.py
+++ urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from blog.views import EntryListView
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', EntryListView.as_view(), name='home'),
 ]
