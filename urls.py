b2ae080cb1dea571c2f2465006000d776e8e1687
Toni Colom <toni@semicolom.com>
2017-11-17 16:07:25 +0100

    Added post detail

--- urls.py
+++ urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from blog import urls as blog_urls
 from blog.views import PostListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', PostListView.as_view(), name='home'),
+    url(r'^blog/', include(blog_urls, namespace='blog')),
 ]
 
 
