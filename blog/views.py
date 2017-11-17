12bdebd8215df4d76ddb09cbcf42e6818cb80c88
Toni Colom <toni.colom@elementsinteractive.es>
2017-11-17 16:07:25 +0100

    Added post detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Post
 
 
 class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
     model = Post
