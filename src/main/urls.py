5a0a0278452e6b213a3c6e7ccb1ffd705fcdd86e
Toni Colom <toni@semicolom.com>
2017-11-26 18:08:29 +0100

    Added home page

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from blog import urls as blog_urls
-from blog.views import PostListView
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', PostListView.as_view(), name='home'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^blog/', include(blog_urls, namespace='blog')),
 ]
 
