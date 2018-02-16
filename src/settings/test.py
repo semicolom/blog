660a88e5f1e6171822c7ef9454c75e345e37b1cc
Toni Colom <toni.colom@elementsinteractive.es>
2018-02-16 18:36:25 +0100

    Added test settings

--- src/settings/test.py
+++ src/settings/test.py
@@ -5,6 +5,6 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': '/var/www/blog/mydatabase.sqlite3',
     }
 }
