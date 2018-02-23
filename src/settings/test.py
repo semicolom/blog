e584a2fcb0bb1a31a81f0cf8f5c5acc3f84884d3
Toni Colom <toni.colom@elementsinteractive.es>
2018-02-23 18:36:16 +0100

    Setted test settings as default

--- src/settings/test.py
+++ src/settings/test.py
@@ -2,9 +2,11 @@ from .base import *
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/www/blog/mydatabase.sqlite3',
-    }
-}
+DEBUG = True
+
+# Database
+DATABASES['default'].update({
+    'USER': 'blog',
+    'PASSWORD': 'blog',
+    'HOST': '127.0.0.1',
+})
