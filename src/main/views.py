46fc988f0a35f3a04ef87ddd63972aaf3b82bc84
Toni Colom <toni@semicolom.com>
2018-09-21 19:42:57 +0200

    Adds contact page

--- src/main/views.py
+++ src/main/views.py
@@ -7,10 +7,8 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
 
-        context.update(dict(
-            last_post=Post.get_last_post()
-        ))
+        context['last_post'] = Post.get_last_post()
 
         return context
