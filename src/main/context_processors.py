9884709dbdbac448804ac9909143085cb3b07e46
Toni Colom <toni@semicolom.com>
2020-03-21 18:58:33 +0100

    Adds service detail

--- src/main/context_processors.py
+++ src/main/context_processors.py
@@ -6,4 +6,7 @@ def site_info(request):
         'AUTHOR_NAME': settings.AUTHOR_NAME,
         'SITE_NAME': settings.SITE_NAME,
         'SITE_URL': settings.SITE_URL,
+        'FLAGS': {
+            'SERVICES_ENABLED': settings.SERVICES_ENABLED,
+        },
     }
